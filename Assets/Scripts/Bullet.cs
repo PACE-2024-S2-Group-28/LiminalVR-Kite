432182f419ddbb1e95b92d10ff49f6333abea4bb
connor.finlayson1@students.mq.edu.au <connor.finlayson1@students.mq.edu.au>
2024-08-26 17:36:04 +1000

    Basic firing implemented

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float timeAlive = 3; 
+    private Transform gun;
+    public Transform Gun {
+        set {
+            gun = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() //move the bullet
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); //move it
+
+        //after a certain time, the bullet fades out and retuns to the gun's list of bullets to fire
+        timeAlive -= Time.deltaTime;
+        if (timeAlive <= 0) {
+            Miss();
+        }
+    }
+
+    private void Miss() {
+        //fading out still to come
+        ReturnToGun();
+    }
+
+    private void OnCollisionEnter(Collision collision) { 
+        //play some particle effect, and destroy meteors
+        ReturnToGun();
+    }
+
+    private void ReturnToGun() {
+        transform.position = gun.position; 
+        gameObject.SetActive(false); //disable the bullet
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    private IVRInputDevice primaryInput;
+    void Start()
+    {
+        //Get the primaryInput
+        primaryInput = VRDevice.Device.PrimaryInputDevice;
+        print("gun woke up");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (primaryInput.GetButtonDown(VRButton.One))
+        {
+            print("fired gun");
+            //Trigger has been pressed and released
+            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.LookRotation(transform.forward, transform.up)); //make the bullet go forward
+            newBullet.GetComponent<Bullet>().Gun = transform;
+        }
+    }
+}
