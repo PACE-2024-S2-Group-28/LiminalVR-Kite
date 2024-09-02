202c43d8592117306f5310ac56b286d3d68514fa
HaonanLin427 <haonan.lin@students.mq.edu.au>
2024-09-02 16:16:12 +1000

    Made the golden asteroid spawned in a fixed position

--- Assets/Scripts/ASpawner.cs
+++ Assets/Scripts/ASpawner.cs
@@ -1,51 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
-public class ASpawner : MonoBehaviour
+public class AsteroidSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] asteroidPrefabs; 
-    [SerializeField] private float spawnRate = 2.0f;
-    [SerializeField] private float goldenChance = 0.05f; 
-    // 5% change to spawn golden Asteroid
+    [SerializeField]
+    private Vector3 spawnBox = Vector3.one;
+
+    [SerializeField]
+    private GameObject[] asteroidFab;
+
+    private GameObject goldenAsteroidInstance;
+
+    [SerializeField]
+    private int maxAsteroids = 7;
+
+    [SerializeField]
+    private float
+        spawnChance = .2f,
+        spawnMinTime = .3f,
+        spawnMaxTime = 2f,
+        spawnRotationStrength = 5f;
+
+    [SerializeField]
+    private int spawnTickRate = 30;
+
+    [SerializeField]
+    private Vector3 goldenAsteroidFixedPosition = new Vector3(0, 5, 0);
+
+    private float timer;
+    private int activeAsteroids = 0;
+    private int goldenAsteroidsCount = 0;
 
     private void Start()
     {
-        StartCoroutine(SpawnAsteroids());
+        InvokeRepeating(nameof(TrySpawn), 0f, 1f / (float)spawnTickRate);
     }
 
-    IEnumerator SpawnAsteroids()
+    private void OnEnable()
     {
-        while (true)
+        RockDestroyer.SEvent_RockDestroyed.AddListener(CountRockDestroyed);
+    }
+
+    private void OnDisable()
+    {
+        RockDestroyer.SEvent_RockDestroyed.RemoveListener(CountRockDestroyed);
+    }
+
+    private void CountRockDestroyed() { activeAsteroids--; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (goldenAsteroidInstance != null)
         {
-            yield return new WaitForSeconds(spawnRate);
-            SpawnAsteroid();
+            Rigidbody rb = goldenAsteroidInstance.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.angularVelocity = new Vector3(0, 1, 0) * spawnRotationStrength; 
+            }
         }
     }
 
-    void SpawnAsteroid()
+    private void TrySpawn()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), 0);
+        if (activeAsteroids >= maxAsteroids) {
+            timer = 0;
+            return;
+        }
+
+        if (timer >= spawnMaxTime) {
+            SpawnAsteroid();
+            return;
+        }
+        
+        if (timer > spawnMinTime) {
+            if(Random.Range(0f, 1f)<=spawnChance) {
+                SpawnAsteroid();
+            }
+        }
+    }
+
+    [Button]
+    private void SpawnAsteroid()
+    {
+        #if UNITY_EDITOR
+        if (!Application.isPlaying) return;
+        #endif
+
+        int prefabIndex = (goldenAsteroidsCount < 1 && activeAsteroids >= 6) ? 1 : 0;
+        if (prefabIndex == 1) goldenAsteroidsCount++;
+        GameObject asteroid = asteroidFab[prefabIndex];
+        
+        activeAsteroids++;
+        timer = 0f;
+
+        Vector3 spawnVec;
+        float x, y, z;
+
+        if (prefabIndex == 1) {
+            spawnVec = goldenAsteroidFixedPosition;
+        } else {
+        //spawn pos random
+         x = Mathf.PerlinNoise(0, Time.time);
+         y = Mathf.PerlinNoise(1, Time.time);
+         z = Mathf.PerlinNoise(2, Time.time);
+        spawnVec = new Vector3(x, y, z) * 2f - Vector3.one;
+        spawnVec = Vector3.Scale(spawnVec, spawnBox);
+        }
+
+        var asteroidT = GameObject.Instantiate(asteroidFab[prefabIndex]).transform;
+        if (prefabIndex == 1)
+        {
+            goldenAsteroidInstance = asteroidT.gameObject; 
+        }
+        asteroidT.parent = this.transform;
+        asteroidT.position = transform.position + spawnVec;
 
 
-        GameObject asteroidPrefab = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
-        //pick one asteroidPrefab
-        GameObject asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
-
-    
-        Renderer renderer = asteroid.GetComponent<Renderer>();
+        Color asteroidColor = (prefabIndex == 0) ? Color.red : Color.yellow; 
+        // Assuming index 0 is red, 1 is golden
+        MeshRenderer renderer = asteroidT.GetComponentInChildren<MeshRenderer>();
         if (renderer != null)
         {
-            if (Random.value < goldenChance)
-            {
-                renderer.material.color = Color.yellow; 
-                // gold Asteroid
-            }
-            else
-            {
-                renderer.material.color = Color.red; 
-                // Red Asteroid
-            }
+            renderer.material.color = asteroidColor;
         }
+
+        //spawn rot random
+        x = Mathf.PerlinNoise(3, Time.time);
+        y = Mathf.PerlinNoise(4, Time.time);
+        z = Mathf.PerlinNoise(5, Time.time);
+        spawnVec = new Vector3(x, y, z) * 360f;
+
+        asteroidT.rotation = Quaternion.Euler(spawnVec);
+
+        //angular velocity random
+        var rb = asteroidT.GetComponentInChildren<Rigidbody>();
+        x = Mathf.PerlinNoise(6, Time.time);
+        y = Mathf.PerlinNoise(7, Time.time);
+        z = Mathf.PerlinNoise(8, Time.time);
+        spawnVec = new Vector3(x, y, z) * 2f - Vector3.one;
+
+        rb.angularVelocity = spawnVec * spawnRotationStrength;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * spawnBox.x);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * spawnBox.y);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * spawnBox.z);
     }
 }
