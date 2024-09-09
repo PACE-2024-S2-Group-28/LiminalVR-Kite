382bb94da540c54d53485f41739fe56d21501da1
iWillBiteRawCoffeeBeans <70265768+iWillBiteRawCoffeeBeans@users.noreply.github.com>
2024-09-09 11:53:57 +1000

    Finished basic functionality of UI panel

--- Assets/Scripts/UI/ScoreDisplayTMPro.cs
+++ Assets/Scripts/UI/ScoreDisplayTMPro.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 public class ScoreDisplayTMPro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private TextMeshPro scoreText;
 
-    // Update is called once per frame
+    [SerializeField]
+    private string scorePrefix;
+
+    private int displayScore;
+
     void Update()
     {
-        
+        displayScore = Mathf.FloorToInt(Time.time);
+
+        scoreText.text = scorePrefix + displayScore;
     }
 }
---
+++ Assets/Scripts/UI/UpgradeBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UpgradeBarController : MonoBehaviour
+{
+    [SerializeField][Range(0,1)]
+    private float fillAmount;
+
+    private Vector3 fillV3;
+
+    [SerializeField]
+    private Transform barFillTransform;
+
+    void Update()
+    {
+        fillV3 = new Vector3(fillAmount, 1, 1);
+        barFillTransform.localScale = fillV3;
+    }
+}
