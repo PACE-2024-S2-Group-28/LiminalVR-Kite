20c9656a0fba77e433bd1b14cd8a0c66ec257f2a
connor.finlayson1@students.mq.edu.au <connor.finlayson1@students.mq.edu.au>
2024-10-02 20:11:32 +1000

    Turrets rotate then fire beam

--- Assets/Scripts/TurretAim.cs
+++ Assets/Scripts/TurretAim.cs
@@ -7,9 +7,14 @@ public class TurretAim : MonoBehaviour
     [SerializeField] private float shootCooldown = 2; //how long between each shot
     [SerializeField] private float beamTime = 2; //how many seconds it takes to destroy an asteroid
     [SerializeField] private float range;
+    [SerializeField] private float timeToRotate = 0.5f; //how many seconds it takes to rotate to face an asteroid before firing
+    private float rotateTimer;
     private float rechargeTimer;
     private float beamTimer;
     private Transform target = null;
+    private Vector3 targetPoint; //where the asteroid will be when the turret finishes rotating
+    private Quaternion startRotation; //rotation of the transform before it started firing
+    private Quaternion toRotation; //rotation of the transform when facing the asteroid's future position
     private LineRenderer beam; //the laser beam
 
     void Awake() {
@@ -27,11 +32,25 @@ public class TurretAim : MonoBehaviour
         rechargeTimer -= Time.deltaTime;
         if (rechargeTimer <= 0) {
             if (target == null) {
-                FindFiringSolution();
+                FindFiringSolution(); //pick a target to lock onto
             }
-            else {
-                Fire();
+            else { //rotate to face asteroid, then fire a beam at it
+                if (rotateTimer < timeToRotate) {
+                    FaceTarget();
+                }
+                else {
+                    Fire();
+                }                
             }            
+        }
+    }
+
+    private void FaceTarget() {
+        transform.rotation = Quaternion.Slerp(startRotation, toRotation, rotateTimer/timeToRotate);
+        rotateTimer += Time.deltaTime;
+        if (rotateTimer >= timeToRotate) { //activate beam when facing asteroid
+            beamTimer = beamTime;
+            beam.enabled = true;
         }
     }
 
@@ -39,21 +58,30 @@ public class TurretAim : MonoBehaviour
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, range); //check all nearby collisions
         Collider closestTarget = null;
         float closestDistance = range;
+        Vector3 closestSpeed = Vector3.zero;
         float distanceToThis;
-        foreach (var hitCollider in hitColliders) {
-            distanceToThis = Vector3.Distance(transform.position, hitCollider.transform.position);
-            //check that the collider is closest of all checked so far, and that it's an asteroi
-            if ((distanceToThis < closestDistance) && (hitCollider.gameObject.CompareTag("Rock"))) {
-                closestTarget = hitCollider;
-                closestDistance = distanceToThis;
+
+        //want to work out the future closest asteroid. The time in the future we're looking at is the time when an asteroid would be getting destroyed by the beam. This makes them seem like they're looking ahead
+        foreach (var hitCollider in hitColliders) { 
+            if (hitCollider.gameObject.CompareTag("Rock")) { //only target rocks
+                Vector3 targetSpeed = hitCollider.attachedRigidbody.velocity; //speed of asteroid
+                Vector3 rotateTargetPoint = hitCollider.transform.position + (targetSpeed * (timeToRotate + beamTime)); //position asteroid moves to during that time
+                distanceToThis = Vector3.Distance(transform.position, rotateTargetPoint);
+                //check that the collider is closest of all checked so far, and that it's an asteroid
+                if (distanceToThis < closestDistance) {
+                    closestTarget = hitCollider;
+                    closestDistance = distanceToThis;
+                    closestSpeed = targetSpeed;
+                }
             }
         }
-        if (closestTarget != null) { //if you found something to shoot, remember it, and activate the laser beam
+        if (closestTarget != null) { //if you found something to shoot, remember it and the turret's current facing
             target = closestTarget.transform;
-            beamTimer = beamTime;
-            beam.enabled = true;
+            startRotation = transform.rotation;
+            targetPoint = target.position + (closestSpeed * timeToRotate); //position asteroid moves to while turret rotates
+            toRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up); //direction to point to asteroid
         }
-        else {
+        else { //nothing found in range
             Debug.Log("No target within range");
             rechargeTimer = shootCooldown;
         }
@@ -66,10 +94,12 @@ public class TurretAim : MonoBehaviour
             beam.SetPosition(0, transform.position);
             beam.SetPosition(1, target.position);
         }
-        else {
+        else { //destroy rock and disable beam
             beam.enabled = false;            
             rechargeTimer = shootCooldown;
             target.parent.gameObject.GetComponent<RockDestroyer>().ChangeRock(forceDir: transform.forward, hitPos: target.position);
+            target = null;
+            rotateTimer = 0;
         }
     }
 
