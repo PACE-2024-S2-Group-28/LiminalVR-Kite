251f1e131f0970461161058323088c8f26ae2dc1
iWillBiteRawCoffeeBeans <70265768+iWillBiteRawCoffeeBeans@users.noreply.github.com>
2024-10-07 23:26:50 +1100

    Starting to add emission animation for gold astreroid

---
+++ Assets/Scripts/GoldAsteroidMaterialAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldAsteroidMaterialAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer meterRenderer;
+
+
+    [SerializeField]
+    private float meterOffsetMax;
+
+    public void Update()
+    {
+        meterRenderer.material.SetTextureOffset("_MainTex", new Vector2(0.5f, osY));
+    }
+}
